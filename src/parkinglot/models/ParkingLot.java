72e526c825fed8bdb7765a5266d305ab29431923
kbhatia01 <karanbhtiacs@gmail.com>
2024-04-26 08:01:51 +0530

    parking lot

---
+++ src/parkinglot/models/ParkingLot.java
@@ -0,0 +1,69 @@
+package parkinglot.models;
+
+import java.util.List;
+
+public class ParkingLot extends BaseModel{
+    private List<Floor> parkingFloor;
+
+    private List<Gate> gates;
+
+    private String name;
+
+    private String address;
+
+    List<VehicleType> allowedVehicles;
+
+    int capacity;
+
+    private ParkingLotStatus status;
+
+    private SlotAssignmentStrategy slotAssignmentStrategy;
+
+    public SlotAssignmentStrategy getSlotAssignmentStrategy() {
+        return slotAssignmentStrategy;
+    }
+
+    public void setSlotAssignmentStrategy(SlotAssignmentStrategy slotAssignmentStrategy) {
+        this.slotAssignmentStrategy = slotAssignmentStrategy;
+    }
+
+    public List<Floor> getParkingFloor() {
+        return parkingFloor;
+    }
+
+    public void setParkingFloor(List<Floor> parkingFloor) {
+        this.parkingFloor = parkingFloor;
+    }
+
+    public List<Gate> getGates() {
+        return gates;
+    }
+
+    public void setGates(List<Gate> gates) {
+        this.gates = gates;
+    }
+
+    public List<VehicleType> getAllowedVehicles() {
+        return allowedVehicles;
+    }
+
+    public void setAllowedVehicles(List<VehicleType> allowedVehicles) {
+        this.allowedVehicles = allowedVehicles;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ParkingLotStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ParkingLotStatus status) {
+        this.status = status;
+    }
+}
